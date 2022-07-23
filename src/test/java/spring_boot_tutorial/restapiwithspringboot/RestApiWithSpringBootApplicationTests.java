f25116ae43062cefb4bb3ddde3fa6748d128c29f
tranminhwang <quangtmps12135@fpt.edu.vn>
2022-07-23 19:13:53 +0700

    refactor test

--- src/test/java/spring_boot_tutorial/restapiwithspringboot/RestApiWithSpringBootApplicationTests.java
+++ src/test/java/spring_boot_tutorial/restapiwithspringboot/RestApiWithSpringBootApplicationTests.java
@@ -2,7 +2,6 @@ package spring_boot_tutorial.restapiwithspringboot;
 
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -13,6 +12,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 
 class RestApiWithSpringBootApplicationTests {
+    Customer genMockCustomer(String firstName, String lastName) {
+        Customer customer = new Customer();
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        return customer;
+    }
+
     @Mock
     private CustomerRepository repository;
     private  CustomerService underTest;
@@ -33,21 +39,22 @@ class RestApiWithSpringBootApplicationTests {
     @Test
     void it_should_add_customer() {
         // when
-        Customer tempCustomer = new Customer();
-        tempCustomer.setFirstName("Quang");
-        tempCustomer.setLastName("Minh Tran");
-        underTest.addCustomer(tempCustomer);
+        Customer mockCustomer = genMockCustomer("Wang", "Minh Tran");
+        underTest.addCustomer(mockCustomer);
         // then
         ArgumentCaptor<Customer> customerArgumentCaptor =
                 ArgumentCaptor.forClass(Customer.class);
         Mockito.verify(repository).save(customerArgumentCaptor.capture());
         Customer capturedCustomer = customerArgumentCaptor.getValue();
-        AssertionsForClassTypes.assertThat(capturedCustomer).isEqualTo(tempCustomer);
+        AssertionsForClassTypes.assertThat(capturedCustomer).isEqualTo(mockCustomer);
     }
 
     @Test
-    @Disabled
     void find_customer_id() {
+        // when
+        Customer mockCustomer =  genMockCustomer("Wang", "Minh Tran");
+        underTest.addCustomer(mockCustomer);
+        // then
+        // ...waiting for me
     }
-
 }
